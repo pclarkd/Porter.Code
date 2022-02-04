d70698d3a59e987ddd84a7b0c9a44b39a6fdd528
pclarkd <pclarkdavisi@gmail.com>
2022-02-04 08:55:17 -0800

    CS11

---
+++ CS11/secondGo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World,")
+
+	fmt.Print("My name is Porter")
+
+	fmt.Print("\nMy favorite fictional character is Tyler Durden...")
+	fmt.Print("\nMy least favorite food is olives")
+	fmt.Print("\n\nAre you kidding me? \n obviously Star Wars!")
+
+}
